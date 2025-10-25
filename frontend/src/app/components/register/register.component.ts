a5d23120e41ae033102efa980ddfd256e8b69a6f
Kyzer <kyzereye@gmail.com>
2025-10-25 11:52:39 -0600

    removed hard coded username and pass

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -31,10 +31,10 @@ export class RegisterComponent {
     private snackBar: MatSnackBar
   ) {
     this.registerForm = this.fb.group({
-      name: ['Jeff', [Validators.required, Validators.minLength(2)]],
-      email: ['kyzereye@gmail.com', [Validators.required, Validators.email]],
-      password: ['1qazxsw2!QAZ', [Validators.required, this.passwordStrengthValidator.bind(this)]],
-      confirmPassword: ['1qazxsw2!QAZ', Validators.required]
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.passwordStrengthValidator.bind(this)]],
+      confirmPassword: ['', Validators.required]
     }, { validators: this.passwordMatchValidator });
 
     // Watch password changes to update requirements checklist
